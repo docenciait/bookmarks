17febb018c02908f4555076cbd31ffcfc877d44a
ivanjimenez <ivanjimenezfp@gmail.com>
2025-02-14 12:08:34 +0100

    .

--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
 def user_login(request):
-    pass
+    return HttpResponse("Fake login")
